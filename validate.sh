eec933d594a2340a119302abf57e1d100acc617b
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2016-05-11 09:42:18 +0200

    Validation procedure, with bash script and Docker image

--- validate.sh
+++ validate.sh
@@ -4,26 +4,46 @@
 # This is a bit brutal (and will affect your system if you are running other
 # containers than those of the lab)
 #
+echo ""
+echo ""
+echo "*** Killing all running containers"
+echo ""
 docker kill $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
 #
-# Remove the docker images
+# Let's get rid of existing images...
 #
+echo ""
+echo ""
+echo "*** Deleting our 3 Docker images, if they exist"
+echo ""
 docker rmi res/auditor
 docker rmi res/musician
-
+docker rmi res/validate-music
+	
 #
-# Build our 2 docker images
-#	
+# ... and rebuild them
+#
+echo ""
+echo ""
+echo "*** Rebuilding our 3 Docker images"
+echo ""
 docker build --tag res/musician --file ./docker/image-musician/Dockerfile ./docker/image-musician/
 docker build --tag res/auditor --file ./docker/image-auditor/Dockerfile ./docker/image-auditor/
+docker build --tag res/validate-music --file ./docker/image-validation/Dockerfile ./docker/image-validation/
 
 #
-# Start 4 containers
+# We start a single container. The Node.js application executed in this container will use
+# a npm package to control Docker. It will start/stop musician and auditor containers and check that
+# the auditor works as expected.
 #
-docker run -d --name res_auditor -p 2205:2205 res/auditor
-docker run -d --name res_musician_1 res/musician piano
-docker run -d --name res_musician_2 res/musician flute
-docker run -d --name res_musician_3 res/musician flute
+echo ""
+echo ""
+echo "*** Starting validation..."
+echo ""
+git remote -v | tee check.log
+docker run --name res_validation -v /var/run/docker.sock:/var/run/docker.sock res/validate-music | tee -a check.log
+
+
 
